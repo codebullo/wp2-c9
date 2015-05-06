6d94aa8a5b753e54768b9888d3717450469f61b2
codebullo <bulloart@gmail.com>
2015-05-06 17:45:39 +0000

    Primer commit

---
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -0,0 +1,15 @@
+
+
+
+<?php
+add_action( 'wp_enqueue_scripts', 'prefix_enqueue_awesome' );
+/**
+ * Register and load font awesome CSS files using a CDN.
+ *
+ * @link   http://www.bootstrapcdn.com/#fontawesome
+ * @author FAT Media
+ */
+function prefix_enqueue_awesome() {
+	wp_enqueue_style( 'prefix-font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
+}
+
---
+++ wp-content/themes/twentythirteen-child/searchform.php
@@ -0,0 +1,14 @@
+
+<div class="cajabuscar">
+
+<form role="search" method="get" class="formbuscar" action="<?php echo home_url( '/' ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		
+		<input type="search" class="inputbuscar" placeholder="<?php echo esc_attr_x( 'Buscar …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+	</label>
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+</form>
+
+</div>
+
